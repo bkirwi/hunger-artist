b4f3d28ef30d251f88631693c3de839350b02e86
Ben Kirwin <ben@kirw.in>
2016-03-29 00:32:58 -0400

    Test ListOffset

--- src/test/scala/com/monovore/hunger/TestScript.scala
+++ src/test/scala/com/monovore/hunger/TestScript.scala
@@ -10,7 +10,7 @@ import org.apache.kafka.common.metrics.Metrics
 import org.apache.kafka.common.network.PlaintextChannelBuilder
 import org.apache.kafka.common.record.{CompressionType, MemoryRecords}
 import org.apache.kafka.common.requests.FetchRequest.PartitionData
-import org.apache.kafka.common.requests.{FetchRequest, MetadataRequest, ProduceRequest}
+import org.apache.kafka.common.requests.{ListOffsetRequest, FetchRequest, MetadataRequest, ProduceRequest}
 import org.apache.kafka.common.utils.SystemTime
 
 import scala.collection.JavaConverters._
@@ -30,13 +30,19 @@ object TestScript {
     val records = MemoryRecords.emptyRecords(ByteBuffer.allocate(1000000), CompressionType.NONE)
     records.append(0L, Array.empty[Byte], Array.empty[Byte])
     records.close()
-    val buffer = records.buffer()
 
+    val partition = new TopicPartition("foo", 0)
+
+    val buffer = records.buffer()
     val result = client.metadata(new MetadataRequest(Collections.singletonList("foo"))).get()
     println(result)
-    val produced = client.produce(new ProduceRequest(-1, 1000, Map(new TopicPartition("foo", 0) -> buffer).asJava)).get()
+    val produced = client.produce(new ProduceRequest(-1, 1000, Map(partition -> buffer).asJava)).get()
     println(produced)
-    val fetched = client.fetch(new FetchRequest(100, 0, Map(new TopicPartition("foo", 0) -> new PartitionData(0, 1000000)).asJava)).get()
+    val fetched = client.fetch(new FetchRequest(100, 0, Map(partition -> new PartitionData(0, 1000000)).asJava)).get()
     println(fetched)
+    val offsets = client.listOffsets(new ListOffsetRequest(
+      Map(partition -> new ListOffsetRequest.PartitionData(-1, 1)).asJava
+    )).get()
+    println(offsets)
   }
 }
